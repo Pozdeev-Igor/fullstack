f472585e3ca037cbac1612cee5ae75ec9c935fa3
Igor <Sketch_90@mail.ru>
2022-10-13 17:19:57 +0300

    Added Offcanvas for keeping comments 2

---
+++ front-end/src/Offcanvas/CommentsContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Offcanvas from "react-bootstrap/Offcanvas";
+
+const CommentsContainer = (props) => {
+    const {show, handleClose, handleShow} = props
+
+    return (
+        <Offcanvas placement="end" scroll="true" backdrop="false" show={show} onHide={handleClose}>
+            <Offcanvas.Header closeButton>
+                <Offcanvas.Title>Комментарии</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+                
+            </Offcanvas.Body>
+        </Offcanvas>
+    );
+};
+
+export default CommentsContainer;
